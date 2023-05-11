cf1888ba6c37c611f3ff17da8a4d8f6b064a4907
Jin-Asanami <is01.njb@gmail.com>
2023-05-11 22:38:04 +0900

    add srcinfo

---
+++ srcinfo.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+cd add-pacman-repository
+makepkg --printsrcinfo > .SRCINFO
+cd ../anthy-unicode
+makepkg --printsrcinfo > .SRCINFO
+cd ../anthy-unicode-git
+makepkg --printsrcinfo > .SRCINFO
+cd ../calamares-settings-univalent
+makepkg --printsrcinfo > .SRCINFO
+cd ../fcitx-anthy-unicode
+makepkg --printsrcinfo > .SRCINFO
+cd ../fcitx5-anthy-unicode
+makepkg --printsrcinfo > .SRCINFO
+cd ../fcitx5-anthy-unicode-git
+makepkg --printsrcinfo > .SRCINFO
+cd ../filesystem
+makepkg --printsrcinfo > .SRCINFO
+cd ../ibus-anthy-unicode
+makepkg --printsrcinfo > .SRCINFO
+cd ../ipw2100-fw
+makepkg --printsrcinfo > .SRCINFO
+cd ../ipw2200-fw
+makepkg --printsrcinfo > .SRCINFO
+cd ../kasumi
+makepkg --printsrcinfo > .SRCINFO
+cd ../kasumi-unicode
+makepkg --printsrcinfo > .SRCINFO
+cd ../kasumi-unicode-git
+makepkg --printsrcinfo > .SRCINFO
+cd ../lsb-release
+makepkg --printsrcinfo > .SRCINFO
+cd ../moeclock
+makepkg --printsrcinfo > .SRCINFO
+cd ../ms-online-apps
+makepkg --printsrcinfo > .SRCINFO
+cd ../plymouth-theme-uvgl-logo
+makepkg --printsrcinfo > .SRCINFO
+cd ../qlipper
+makepkg --printsrcinfo > .SRCINFO
+cd ../srcinfo.sh
+makepkg --printsrcinfo > .SRCINFO
+cd ../unifetch
+makepkg --printsrcinfo > .SRCINFO
+cd ../univag-restricted-extras
+makepkg --printsrcinfo > .SRCINFO
+cd ../univalent-keyring
+makepkg --printsrcinfo > .SRCINFO
+cd ../univalent-logo
+makepkg --printsrcinfo > .SRCINFO
+cd ../univalent-mirrorlist
+makepkg --printsrcinfo > .SRCINFO
+cd ../univalent-tools
+makepkg --printsrcinfo > .SRCINFO
+cd ../univalive
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-cinnamon-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-cinnamon-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-gnome-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-gnome-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-i3wm-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-i3wm-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-lxqt-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-lxqt-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-lxqt-kamu-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-lxqt-kamu-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-mate-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-mate-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-plasma-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-plasma-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-unity-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-unity-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-xfce-en
+makepkg --printsrcinfo > .SRCINFO
+cd ../uvgl-skel-xfce-ja
+makepkg --printsrcinfo > .SRCINFO
+cd ../wine-desktop-entries
+makepkg --printsrcinfo > .SRCINFO
+cd ../wps-office-mui-ja-jp
+makepkg --printsrcinfo > .SRCINFO
+cd ../
